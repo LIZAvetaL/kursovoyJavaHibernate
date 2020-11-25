dc740fbb65505557c9616b55ba2861d903ca0673
lizavetal <lizaveta.levanovich@gmail.com>
2020-11-25 23:07:53 +0300

    fix admin

--- src/main/java/Server/Commands/UserCommands.java
+++ src/main/java/Server/Commands/UserCommands.java
@@ -1,12 +1,14 @@
 package Server.Commands;
 
 import Server.Database.HibernateSessionFactoryUtil;
+import Server.Model.AdminEntity;
 import Server.Model.BasketEntity;
 import Server.Model.UsersEntity;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserCommands {
@@ -31,8 +33,52 @@ public class UserCommands {
                 commands = command.split(",", 4);
                 result = UserCommands.addClient(commands[2], commands[3]);
                 break;
+            case "addAdmin":
+                commands = command.split(",", 4);
+                result = UserCommands.addAdmin(commands[2], commands[3]);
+                break;
+            case "deleteUser":
+                commands = command.split(",", 3);
+                result = UserCommands.deleteUser(commands[2]);
+                break;
+            case "showUser":
+                result = UserCommands.showUser();
+                break;
         }
         return result;
+    }
+
+    private static String addAdmin(String login, String password) {
+        UsersEntity user = new UsersEntity();
+        AdminEntity admin=new AdminEntity();
+        user.setLogin(login);
+        user.setPassword(password);
+        admin.setUser(user);
+        UserCommands.save(admin);
+        return "success";
+    }
+
+    private static String deleteUser(String idUserString) {
+        int idUSer= Integer.parseInt(idUserString);
+        Session session=HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        List<UsersEntity> list=session.createQuery("from UsersEntity  where id_user=:id").setParameter("id",idUSer).list();
+        session.close();
+        UsersEntity user=list.get(0);
+        UserCommands.delete(user);
+        return "success";
+    }
+
+    private static Object showUser() {
+        List<UsersEntity> users=HibernateSessionFactoryUtil.getSessionFactory().openSession().
+                createQuery("from UsersEntity").list();
+        ArrayList<String>list=new ArrayList();
+        for (UsersEntity user:users){
+            String id= String.valueOf(user.getIdUser());
+            String login=user.getLogin();
+            String password=user.getPassword();
+            list.add(id+","+login+","+password);
+        }
+        return list;
     }
 
     private static String addClient(String login, String password) {
@@ -90,10 +136,17 @@ public class UserCommands {
         return message;
     }
 
-    private static void save(UsersEntity user) {
+    private static void save(Object user) {
         Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
         Transaction tx1 = session.beginTransaction();
         session.save(user);
+        tx1.commit();
+        session.close();
+    }
+    private static void delete(UsersEntity user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.delete(user);
         tx1.commit();
         session.close();
     }
